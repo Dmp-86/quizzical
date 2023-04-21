31eb20c495401c754a02c05e7f3ac34d53c027fd
Dmp-86 <danni_power69@hotmail.com>
2023-04-21 11:13:15 +0000

    Define function & link to API

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// 
+// https://opentdb.com/api.php?amount=10
+
+
+async function loadQuestion(){
+    const APIUrl ='https://opentdb.com/api.php?amount=1' ;
+    const result = await fetch(`${APIUrl}`);
+    const data = await result.json();
+    console.log(data.results[0]);
+}
+
+loadQuestion();
