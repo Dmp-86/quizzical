ec49a3ddaaa63b286601d3e0a17a4a90a50f2f04
Dmp-86 <danni_power69@hotmail.com>
2023-04-22 11:00:35 +0000

    Fix option selector

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-// https://opentdb.com/api.php?amount=10
+// https://opentdb.com/api.php?amount=10&type=multiple
 
 const _question = document.getElementById('question');
 const _options = document.querySelector('.quiz-options');
+const _correctScore = document.getElementById('correct-score');
+const _totalQuestion = document.getElementById('total-question');
+
+
+let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 10;
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadQuestion();
+    _totalQuestion.textContent = totalQuestion;
+    _correctScore.textContent = correctScore;
+});
+
 
 
 async function loadQuestion(){
-    const APIUrl ='https://opentdb.com/api.php?amount=1' ;
+    const APIUrl ='https://opentdb.com/api.php?amount=10&type=multiple' ;
     const result = await fetch(`${APIUrl}`);
     const data = await result.json();
     // console.log(data.results[0]);
     showQuestion(data.results[0]);
 }
 
+// display question and options
 function showQuestion(data){
-    let correctAnswer = data.correct_answer;
+    correctAnswer = data.correct_answer;
     let incorrectAnswer = data.incorrect_answers;
     let optionsList = incorrectAnswer;
     optionsList.splice(Math.floor(Math.random() * (incorrectAnswer.length +1)), 0, correctAnswer); 
@@ -24,6 +38,19 @@ function showQuestion(data){
         <li> ${index + 1}. <span> ${option} </span> </li>
         `).join('')}
     `;
+
+    selectOption();
 }
 
-loadQuestion();
+// option selection
+function selectOption(){
+    _options.querySelectorAll('li').forEach((option) => {
+        option.addEventListener('click', () => {
+            if(_options.querySelector('.selected')){
+                const activeOption = _options.querySelector('.selected');
+                activeOption.classList.remove('selected');
+            }
+            option.classList.add('selected');
+        });
+    });
+}
