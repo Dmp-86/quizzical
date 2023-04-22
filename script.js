bf9b0f766567e49e537a25258acf8be50ce22cc2
Dmp-86 <danni_power69@hotmail.com>
2023-04-22 11:59:25 +0000

    Debug check answer function

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const _question = document.getElementById('question');
 const _options = document.querySelector('.quiz-options');
 const _correctScore = document.getElementById('correct-score');
 const _totalQuestion = document.getElementById('total-question');
-
+const _checkBtn = document.getElementById('check-answer');
+const _playAgainBtn = document.getElementById('play-again')
 
 let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 10;
 
+// event listeners
+function eventListeners(){
+    _checkBtn.addEventListener('click', checkAnswer);
+
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     loadQuestion();
+    eventListeners();
     _totalQuestion.textContent = totalQuestion;
     _correctScore.textContent = correctScore;
 });
@@ -53,4 +60,19 @@ function selectOption(){
             option.classList.add('selected');
         });
     });
+    console.log(correctAnswer);
+}
+
+// answer checking 
+function checkAnswer(){
+    _checkBtn.disabled = true;
+    if(_options.querySelector('.selected')){
+        let selectedAnswer = options.querySelector('.selected span').textContent;
+        if(selectedAnswer === correctAnswer){
+            correctScore++;
+            _result.innerHTML = `<p> <i class = "fas fa-check"></i>Correct Answer! </p>`;
+        } else {
+            _result.innerHTML = `<p> <i class = "fas fa-times"></i>Incorrect Answer! <small><b>Correct Answer: </b> ${correctAnswer}</small></p>`;
+        }
+    }
 }
